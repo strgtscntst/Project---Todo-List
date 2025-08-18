a4e30248d60eb293b7ef7254fe7f52bbb98baae2
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2025-08-18 16:53:39 -0400

    feat Project and ToDo functioanlity
    - Added Project constructor to create project objects
    - Added createToDo method to add ToDo items within projects
    - Kept ToDo object constructor separate for general use

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 console.log("Connection")
 
 const projects = []
-const toDos = []
+const content = document.getElementById("content")
 
 function populateContent(projects){
     projects.forEach(project => {
-        project.forEach(toDo => populateToDo(toDo))
+        project.toDos.forEach(toDo => populateToDo(toDo))
     });
 }
 
@@ -18,36 +18,55 @@ function populateToDo(toDo){
     console.log(`Notes: ${toDo.notes}`)
 }
 
-function ToDo(title, description, dueDate, priority, notes){
-        this.title = title,
-        this.description = description,
-        this.dueDate = dueDate,
-        this.complete = false,
-        this.priority = priority,
-        this.notes = notes
+function Project(projectName){
+ 
+    this.projectName = projectName,
+    this.complete = false,
+    this.toDos = []
+    this.createToDo = (title, description, dueDate, priority, notes)=>{
+        this.toDos.push(new ToDo(title, description, dueDate, priority, notes))
+    }
+
+    this.createToDo = function(title, description, dueDate, priority, notes){
+        this.toDos.push({
+            title: title,
+            description: description,
+            dueDate: dueDate,
+            complete: false,
+            priority: priority,
+            notes: notes
+        })
+    }
+
 
 }
 
-// function Project(){
-//     this.createToDo = createToDo()    
-// }
+function ToDo(title, description, dueDate, priority, notes){
+    this.title = title,
+    this.description = description,
+    this.dueDate = dueDate,
+    this.complete = false,
+    this.priority = priority,
+    this.notes = notes
+}
 
+// test content
 
-//test content
-// const toDoTest = {
-//     title: "Figure shit out",
-//     description: "This should be able to be as long as it needs to be. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ,
-//     complete: false,
-//     dueDate: "10/26/2025",
-//     priority: 5,
-//     notes: "This is a test ToDo.",
-// }
-
-const testProject = [
-//     toDoTest,
-]
+const testProject = new Project("Test Project")
+testProject.createToDo(
+    "Figure shit out", 
+    "This should be able to be as long as it needs to be. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+    "10/26/2025", 
+    5, 
+    "This is a test ToDo." )
+testProject.createToDo(
+    "Test a second TODO", 
+    "This should be able to be as long as it needs to be. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+    "4/10/2025", 
+    1, 
+    "This is another test ToDo." )
 
 //test scripts
-testProject.push(new ToDo("first", "second", "third", 4, "fifth"))
+console.log(`Project Name: ${testProject.projectName}`)
 projects.push(testProject)
 populateContent(projects)
